5c72f8d15ce5435d2a012f2d459923a4b9311b80
mebratukumera <45142243+mebratukumera@users.noreply.github.com>
2019-03-29 18:41:09 +0300

    Add files via upload

---
+++ build/precache-manifest.5b4a16404a22e2fcef841a26f36792b0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ade6b88019b53c697b7",
+    "url": "/static/css/main.7913f078.chunk.css"
+  },
+  {
+    "revision": "1ade6b88019b53c697b7",
+    "url": "/static/js/main.cb46ee46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5f133c1f0f99c26afb7",
+    "url": "/static/css/2.0f70fa0e.chunk.css"
+  },
+  {
+    "revision": "e5f133c1f0f99c26afb7",
+    "url": "/static/js/2.cb6b0be9.chunk.js"
+  },
+  {
+    "revision": "8e94bbb03de8e743523e8e4385bf392b",
+    "url": "/static/media/back.8e94bbb0.svg"
+  },
+  {
+    "revision": "4c39b3535a6cae243d1a318eb5600a50",
+    "url": "/static/media/education.4c39b353.svg"
+  },
+  {
+    "revision": "e581b1c60e88376868f294be6aead68e",
+    "url": "/index.html"
+  }
+];
